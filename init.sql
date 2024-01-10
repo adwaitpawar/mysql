b96dd034c2c8afd532af3d4d526b5dab4b4f1636
adwaitpawar <110279221+adwaitpawar@users.noreply.github.com>
2024-01-10 10:05:04 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,1 +1,12 @@
+-- Create the EmployeeDB database
+CREATE DATABASE IF NOT EXISTS EmployeeDB;
+
+-- Switch to the EmployeeDB database
+USE EmployeeDB;
+
+-- Create the Department table
+CREATE TABLE IF NOT EXISTS Department (
+    DepartmentId INT PRIMARY KEY AUTO_INCREMENT,
+    DepartmentName NVARCHAR(255) NOT NULL
+);
 
